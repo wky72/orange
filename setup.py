05fbf37b1a546bc5098a8d9adcfbc9a2a9820d1b
wukaiyu@roobo.com <wukaiyu@roobo.com>
2019-07-04 16:55:55 +0800

    xx

---
+++ setup.py
@@ -0,0 +1,13 @@
+# from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name="orange",
+      version='0.0.1',
+      description='python tools',
+      author='wky',
+      author_email='1351654450@qq.com',
+      url='https://github.com/wky72/orange.git',
+      license='MIT',
+      install_requires="re",
+      packages=find_packages(exclude=["logs"]),)
