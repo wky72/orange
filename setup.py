17240ac39eddebd70f01fd04c12d8f7abc2a50fb
wukaiyu@roobo.com <wukaiyu@roobo.com>
2019-07-04 17:29:18 +0800

    xx

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name="orange",
       author_email='1351654450@qq.com',
       url='https://github.com/wky72/orange.git',
       license='MIT',
-      install_requires="re>2.2.0",
+      install_requires=["re>=2.2.1",],
       packages=find_packages(exclude=["logs"]),)
